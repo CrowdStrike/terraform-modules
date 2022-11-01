76caf7af242185b4b4390f7021e16a6023ed6bdd
Ryan Payne <ryanj.payne@outlook.com>
2022-11-01 11:26:52 -0500

    remove provider

--- falcon/falcon-operator/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "kubernetes" {
-  host                   = var.endpoint
-  client_certificate     = var.client-cert
-  client_key             = var.client-key
-  cluster_ca_certificate = var.ca-cert
-}
