ad9288413106d2c7c519c50d8cfd5a3872e8aac7
Ryan Payne <ryanj.payne@outlook.com>
2022-11-07 08:21:47 -0600

    Update helm.tf

--- falcon/k8s-protection-agent/helm.tf
+++ falcon/k8s-protection-agent/helm.tf
@@ -1,14 +1,3 @@
-provider "helm" {
-  kubernetes {
-    host                   = var.cluster-endpoint
-    cluster_ca_certificate = base64decode(var.cluster-ca-cert)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster-name]
-      command     = "aws"
-    }
-  }
-}
 
 # run this first: `helm repo add kpagent-helm https://registry.crowdstrike.com/kpagent-helm && helm repo update`
 resource "helm_release" "kpagent" {
