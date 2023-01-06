0c9874b8ecf9a6b30c09c74877a2ace37e9a8006
Ryan Payne <ryanj.payne@outlook.com>
2023-01-06 13:33:31 -0600

    add resource_labels to cluster config

--- gcp/gke/main.tf
+++ gcp/gke/main.tf
@@ -1,33 +1,35 @@
 resource "google_container_cluster" "gke_cluster_autopilot" {
-  count = var.autopilot == true ? 1 : 0
-  name     = "${var.alias}-${var.cluster_name}"
-  location = var.region
-  enable_autopilot = true
+  count              = var.autopilot == true ? 1 : 0
+  name               = "${var.alias}-${var.cluster_name}"
+  location           = var.region
+  enable_autopilot   = true
+  initial_node_count = 1
+  network            = var.vpc_name
+  subnetwork         = var.subnet_name
+  resource_labels    = var.resource_labels
+  
   ip_allocation_policy {
   }
-  initial_node_count       = 1
-  network    = var.vpc_name
-  subnetwork = var.subnet_name
 }
 
 resource "google_container_cluster" "gke_cluster" {
-  count = var.autopilot == false ? 1 : 0
-  name     = "${var.alias}-${var.cluster_name}"
-  location = var.region
+  count                    = var.autopilot == false ? 1 : 0
+  name                     = "${var.alias}-${var.cluster_name}"
+  location                 = var.region
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  network                  = var.vpc_name
+  subnetwork               = var.subnet_name
+  resource_labels          = var.resource_labels  
   
   # We only use separately managed node pools. 
   # So we create the smallest possible default
   # node pool and immediately delete it.
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  network    = var.vpc_name
-  subnetwork = var.subnet_name
 }
 
 # Separately Managed Node Pool
 resource "google_container_node_pool" "gke_nodes" {
-  count = var.autopilot == false ? 1 : 0
+  count      = var.autopilot == false ? 1 : 0
   name       = "${var.alias}-gke-nodepool"
   location   = var.region
   cluster    = google_container_cluster.gke_cluster[0].name
@@ -38,13 +40,12 @@ resource "google_container_node_pool" "gke_nodes" {
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
-
     labels = {
       env = var.alias
     }
-    image_type = var.node_os
+    image_type   = var.node_os
     machine_type = "n1-standard-1"
-    tags         = ["${var.alias}-gke-node", "${var.alias}-gke-cluster"]
+    tags         = ["${var.alias}-gke-node", "${var.alias}-gke-cluster"]    
     metadata = {
       disable-legacy-endpoints = "true"
     }
