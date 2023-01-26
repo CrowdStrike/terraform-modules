0895ea7991d108670491b462637590a1cf06a715
ffalor <35144141+ffalor@users.noreply.github.com>
2023-01-26 10:33:39 -0600

    remove helm requirements (#13)

--- operator/variables.tf
+++ operator/variables.tf
@@ -1,30 +1,29 @@
 # Falcon sensor type
 # Allowed Values: FalconNodeSensoror FalconContainer
 variable "sensor_type" {
-    type = string
-    default = "FalconNodeSensor"
-    description = "Falcon sensor type: FalconNodeSensor or FalconContainer"
+  type        = string
+  default     = "FalconNodeSensor"
+  description = "Falcon sensor type: FalconNodeSensor or FalconContainer."
 
-    validation {
-        condition = contains(["FalconNodeSensor", "FalconContainer"], var.sensor_type)
-        error_message = "Sensor type must be FalconNodeSensor or FalconContainer"
-    }
-
+  validation {
+    condition     = contains(["FalconNodeSensor", "FalconContainer"], var.sensor_type)
+    error_message = "Sensor type must be FalconNodeSensor or FalconContainer."
+  }
 }
 
 # Falcon credentials
 variable "client_id" {
-    type = string
-    description = "Falcon API Client ID"
-    sensitive = true
+  type        = string
+  description = "Falcon API Client ID"
+  sensitive   = true
 }
 variable "client_secret" {
-    type = string
-    description = "Falcon API Client Secret"
-    sensitive = true
+  type        = string
+  description = "Falcon API Client Secret"
+  sensitive   = true
 }
 # Environment or 'Alias' tag
 variable "environment" {
-    description = "Environment or 'Alias' tag"
-    type = string
+  description = "Environment or 'Alias' tag"
+  type        = string
 }
